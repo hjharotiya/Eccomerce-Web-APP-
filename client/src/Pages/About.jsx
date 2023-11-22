6857a042368a42d246edb53739f6132b65f6dc01
harshit jharotiya <jharotiyaharshit@gmail.com>
2023-11-22 15:35:15 +0530

    adding SEO functionality

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -1,11 +1,13 @@
 import Layout from "../components/layout/Layout";
 
 const About = () => {
-	return <>
-		<Layout>
-			<h1>About is here</h1>
-			</Layout>
-	</>
-}
+  return (
+    <>
+      <Layout title={"about Us-Ecommerce App"}>
+        <h1>About is here</h1>
+      </Layout>
+    </>
+  );
+};
 
 export default About;
--- client/src/components/layout/Layout.jsx
+++ client/src/components/layout/Layout.jsx
@@ -1,14 +1,29 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import { Helmet } from "react-helmet";
 
-const Layout = ({ children }) => {
+const Layout = ({ children, title, description, keywords, author }) => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords} />
+        <meta name="author" content={author} />
+        <title>{title}</title>
+      </Helmet>
       <Header></Header>
       <main style={{ minHeight: "75vh" }}>{children}</main>
       <Footer></Footer>
     </div>
   );
+};
+
+Layout.defaultProps = {
+  title: "Ecommerce app -shop now",
+  description: "mern stack developer",
+  keywords: "mern,react,ecommerce,node,mongodb",
+  author: "harshit",
 };
 
 export default Layout;
